a7fbd96a92b38dba12eb78bcf4a703c61fd37808
iMoloy <moloykrishnapaul@gmail.com>
2026-03-08 23:23:59 +0600

    Complete the load data script

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ if (signinBtn) {
     }
   });
 }
+
+// Home Page
+const API = "https://phi-lab-server.vercel.app/api/v1/lab";
+
+let allIssues = [];
+let activeFilter = "all";
+
+const modal = document.getElementById("issueModal");
+const issuesGrid = document.getElementById("issuesGrid");
+const issueCount = document.getElementById("issueCount");
+const loadingSpinner = document.getElementById("loadingSpinner");
+const searchInput = document.getElementById("searchInput");
+
+// Load Data
+const loadIssues = () => {
+  loadingSpinner.classList.remove("hidden");
+  issuesGrid.classList.add("hidden");
+
+  fetch(`${API}/issues`)
+    .then((res) => res.json())
+    .then((data) => {
+      allIssues = data.data || data;
+      renderCards(allIssues);
+      loadingSpinner.classList.add("hidden");
+      issuesGrid.classList.remove("hidden");
+    });
+};
