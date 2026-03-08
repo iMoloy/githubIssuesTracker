ad1c4e2f7ef33ae1e5e84e04750b20d276b4fc5d
iMoloy <moloykrishnapaul@gmail.com>
2026-03-08 23:16:54 +0600

    Complete the home page design

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-// Login with the credentials
-document.getElementById("signin").addEventListener("click", () => {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  if (username === "admin" && password === "Admin123") {
-    window.location.href = "home.html";
-  } else {
-    alert("Invalid Username or Password");
-  }
-});
+// Login
+const signinBtn = document.getElementById("signin");
+
+if (signinBtn) {
+  signinBtn.addEventListener("click", () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "Admin123") {
+      window.location.href = "home.html";
+    } else {
+      alert("Invalid Username or Password");
+    }
+  });
+}
